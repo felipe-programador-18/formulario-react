e8fda18fc3508a69600eef0002d2000897e38b01
Felipe <machadofelipe2016@outlook.com>
2022-05-23 17:52:25 -0300

    c5-create button subscribe to make practice about manage form

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "e0e29a0a82db4f09"; }
+/******/ 		__webpack_require__.h = function() { return "28a8871d49acf1cb"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- pages/controlled-date.js
+++ pages/controlled-date.js
@@ -9,7 +9,8 @@ const controlledFor = () => {
     const [form, setform] = useState({
         name:'',
         email:'',
-        uf:''
+        uf:'',
+        subscribe:false
     })
     
     const [show, setshow] = useState(true)
@@ -36,6 +37,22 @@ const controlledFor = () => {
          <input type='text' name='name' value={form.name}  onChange={onChange}  /> <br/>
          Email:
          <input type='text' name='email' value={form.email}  onChange={onChange}  /> <br/>
+         Wish receice trending for email:
+         <input
+          type='checkbox' 
+          name='subscribe'
+          value={form.subscribe}  
+          onChange={onChange}  
+          /> <br/>
+        
+        <select name='uf' onChange={onChange}  value={form.uf}>
+          <option>select uf:</option>
+          { uf.map ( uf => <option value={uf} key={uf} >
+               {uf} 
+          </option> ) }   
+        
+        </select>    
+        
       <button type='button' onClick={getValue} >get value!</button>
       <button type='button' onClick={() => setshow(curr => !curr)} >Hide input!</button>
        <pre> { JSON.stringify(form, null, 2)}</pre>
